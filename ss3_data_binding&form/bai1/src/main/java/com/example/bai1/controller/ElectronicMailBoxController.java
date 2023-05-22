f2aaad715788bae5e60dc9c8764752ba4a68dab3
huỳnh đức định <124021043+huynhducdinh@users.noreply.github.com>
2023-05-22 12:21:59 +0700

    first commit

--- ss3_data_binding&form/bai1/src/main/java/com/example/bai1/controller/ElectronicMailBoxController.java
+++ ss3_data_binding&form/bai1/src/main/java/com/example/bai1/controller/ElectronicMailBoxController.java
@@ -3,31 +3,32 @@ package com.example.bai1.controller;
 import com.example.bai1.model.ElectronicMailBoxModel;
 import com.example.bai1.service.ElectronicMailBoService;
 import com.example.bai1.service.IElectronicMailBoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.context.request.RequestAttributes;
+
 
 @Controller
-
 public class ElectronicMailBoxController {
-    private IElectronicMailBoService iElectronicMailBoService=new ElectronicMailBoService();
+    @Autowired
+    private  IElectronicMailBoService iElectronicMailBoService;
     @GetMapping("/")
     public String viewMailBox (Model model){
         String [] language= new String[]{"English","Vietnamese","Japanese","Chinese"};
         String [] pageSize=new  String[]{ "5","10","15","20","25"};
-//        Boolean checkSpamsFilter;
-
+        ElectronicMailBoxModel electronicMailBoxModel=iElectronicMailBoService.getAll();
         model.addAttribute("language",language);
         model.addAttribute("pageSize",pageSize);
-        model.addAttribute("mailBox",new ElectronicMailBoxModel());
+        model.addAttribute("electronicMailBoxModel",electronicMailBoxModel);
         return "mailbox";
     }
-    @PostMapping("/mailbox")
-    public String Edit(@ModelAttribute("mailBox")ElectronicMailBoxModel electronicMailBoxModel, BindingResult bindingResult ){
-      ElectronicMailBoxModel electronicMailBox =new ElectronicMailBoxModel();
-     return  "mailbox";
+    @PostMapping("/mail")
+    public String Update(@ModelAttribute("mailBox")ElectronicMailBoxModel electronicMailBoxModel ,Model model ){
+        iElectronicMailBoService.save(electronicMailBoxModel);
+        ElectronicMailBoxModel electronicMailBox= iElectronicMailBoService.update();
+        model.addAttribute("electronicMailBox",electronicMailBox);
+        return  "update";
     }
 
 }
